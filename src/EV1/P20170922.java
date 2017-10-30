e52d845e7fb125ae92f74ba389eeeb38a7936c72
SgtNaboloco <pablodiazsjm@hotmail.com>
2017-10-30 20:15:56 +0100

    1st time

---
+++ src/EV1/P20170922.java
@@ -0,0 +1,24 @@
+package EV1;
+
+import java.util.Scanner;
+
+public class P20170922 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime a:");
+		int a=sc.nextInt();
+		System.out.println("Dime b:");
+		int b=sc.nextInt();
+		run(a,b);
+	}
+
+	private static void run(int a, int b) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
---
+++ src/EV1/elRetratoRobot.java
@@ -0,0 +1,10 @@
+package EV1;
+
+public class elRetratoRobot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
