fab00c9f24e82024191b0145e091150aa15380e1
SgtNaboloco <pablodiazsjm@hotmail.com>
2017-11-02 15:24:41 +0100

    snake iniciada

---
+++ src/com/gmail/pablo23dr/snakeGameApplet/Snake.java
@@ -0,0 +1,96 @@
+package com.gmail.pablo23dr.snakeGameApplet;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snake {
+
+	List<Point> snakePoints;
+	
+	int xDir,yDir;
+	boolean isMoving, elongate;
+	final int startSize = 20, startX = 180, startY = 180;
+	
+	
+	public Snake() {
+		// TODO Auto-generated constructor stub
+		snakePoints = new ArrayList<Point>();
+		xDir =0;
+		yDir = 0;
+		
+		isMoving=false;
+		elongate = false;
+		
+		snakePoints.add(new Point(startX,startY));
+		for (int i = 0; i < startSize; i++) {
+			snakePoints.add(new Point(startX-i*4,startY));
+		}
+		
+	}
+	public void draw(Graphics g) {
+		g.setColor(Color.WHITE);
+		
+		for (Point p : snakePoints) {
+			g.fillRect(p.getX(), p.getY(), 4,4);
+		}
+		
+	}
+	
+	public int getxDir() {
+		return xDir;
+	}
+
+	public void setxDir(int xDir) {
+		this.xDir = xDir;
+	}
+
+	public int getyDir() {
+		return yDir;
+	}
+
+	public void setyDir(int yDir) {
+		this.yDir = yDir;
+	}
+
+	public int getX() {
+		return snakePoints.get(0).getX();
+	}
+	public int getY() {
+		return snakePoints.get(0).getY();
+	}
+	
+	public void move() {
+		if (isMoving) {
+			Point temp = snakePoints.get(0);
+			Point last = snakePoints.get(snakePoints.size()-1);
+			Point newStart = new Point (temp.getX() + xDir * 4 ,temp.getY() + yDir *4);
+			for (int i = snakePoints.size()-1; i >=1; i--) {
+				snakePoints.set(i, snakePoints.get(i -1));
+			}
+			snakePoints.set(0, newStart);
+		}
+	}
+	public boolean isMoving() {
+		return isMoving;
+	}
+	public void setMoving(boolean isMoving) {
+		this.isMoving = isMoving;
+	}
+	
+	public boolean snakeCollision() {
+		int x = this.getX();
+		int y = this.getY();
+		
+		for (int i = 1; i < snakePoints.size(); i++) {
+			if (snakePoints.get(i).getX() == x 
+				&& snakePoints.get(i).getY() == y) {
+				return true;
+			}
+			
+		}return false;
+		
+		
+	}
+}
---
+++ src/com/gmail/pablo23dr/snakeGameApplet/SnakeGame.java
@@ -0,0 +1,151 @@
+package com.gmail.pablo23dr.snakeGameApplet;
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class SnakeGame extends Applet implements Runnable, KeyListener {
+
+	Graphics gfx;
+	Image img;
+	Thread thread;
+	Snake snake;
+	boolean GameOver=false;
+
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+
+		this.resize(400, 400);
+		img = createImage(400, 400);
+		gfx = img.getGraphics();
+		
+		this.addKeyListener(this);
+		
+		
+		snake = new Snake();
+
+		thread = new Thread(this);
+		thread.start();
+
+	}
+
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paint(g);
+		gfx.setColor(Color.BLACK);
+		gfx.fillRect(0, 0, 400, 400);
+		if (!GameOver) {
+			snake.draw(gfx);
+		} else {
+			gfx.setColor(Color.RED);
+			gfx.drawString("Game Over", 180, 150);
+		}
+		
+		
+
+		g.drawImage(img, 0, 0, null);
+	}
+
+	public void update(Graphics g) {
+		// TODO Auto-generated method stub
+		super.update(g);
+
+		paint(g);
+	}
+
+	public void repaint(Graphics g) {
+		// TODO Auto-generated method stub
+		super.repaint();
+
+		paint(g);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for (;;) {
+
+			if (!GameOver) {
+				snake.move();
+				this.checkGameOver();
+			}
+			
+			this.repaint();
+			try {
+				Thread.sleep(40);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+
+		if (!snake.isMoving()) {
+			if (e.getKeyCode() == KeyEvent.VK_UP 
+				|| e.getKeyCode() == KeyEvent.VK_DOWN 
+				|| e.getKeyCode() == KeyEvent.VK_RIGHT ) {
+				snake.setMoving(true);
+			}
+		}
+		
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			if (snake.getyDir() != 1) {
+				snake.setyDir(-1);
+				snake.setxDir(0);
+			}
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			if (snake.getyDir() != -1) {
+				snake.setyDir(1);
+				snake.setxDir(0);
+			}
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			if (snake.getxDir() != 1) {
+				snake.setxDir(-1);
+				snake.setyDir(0);
+			}
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			if (snake.getxDir() != -1) {
+				snake.setxDir(1);
+				snake.setyDir(0);
+			}
+		}
+	}
+
+	public void checkGameOver() {
+		if (snake.getX() < 0 ||
+				snake.getX() > 396) {
+			GameOver=true;
+		}
+		if (snake.getY() < 0 ||
+				snake.getY() > 396) {
+			GameOver=true;
+		}
+		
+		if (snake.snakeCollision()) {
+			GameOver=true;
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+}
