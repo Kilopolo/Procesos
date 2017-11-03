b6e493e20652e3867139a962c57373d7ffe220f8
SgtNaboloco <pablodiazsjm@hotmail.com>
2017-11-03 23:33:34 +0100

    draw

--- src/com/gmail/pablo23dr/killTheNazi/HumanShip.java
+++ src/com/gmail/pablo23dr/killTheNazi/HumanShip.java
@@ -1,5 +1,103 @@
 package com.gmail.pablo23dr.killTheNazi;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class HumanShip {
+
+	double x, dirX;
+	boolean leftAccel, rightAccel;
+	int y, proportion;
+	final double GRAVITY = 0.94;
+
+	public HumanShip() {
+
+		x = 40;
+		y = 40;
+		proportion = 4;
+	}
+
+	public void move() {
+
+	}
+
+
+
+	public void draw(Graphics g) {
+		/**
+		 * alas = x,y 0 = 0,0 1 = 0,1 2 = 1,1 3 = 1,3 4 = 2.3 5 = 2.1 6 =3.1 7 = 3.2 8 =
+		 * 4.2 9 = 4.3 10=5.3 11=5.2 12=6.2 13=6.1 14=7.1 15=7.3 16=8.3 17=8.1 18=9.1
+		 * 19=9.0 20=0.0
+		 * 
+		 * cabina =x,y 0 =2.0 1 =2.2 2 =3.2 3 =3.4 4 =4.4 5 =4.6 6 =5.6 7 =5.4 8 =6.4 9
+		 * =6.2 10=7.2 11=7.0 12=2.0
+		 */
+		int xAlasPoints[] = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 0 };
+		int yAlasPoints[] = { 0, 1, 1, 3, 3, 1, 1, 2, 2, 3, 3, 2, 2, 1, 1, 3, 3, 1, 1, 0, 0 };
+		int nAlasPoints = 21;
+		int xAlasProPoints[]= new int[nAlasPoints];
+		int yAlasProPoints[]= new int[nAlasPoints];
+		
+		for (int i = 0; i < yAlasPoints.length; i++) {
+			xAlasProPoints[i]=(int) (getX()+(xAlasPoints[i]*getProportion()));
+			yAlasProPoints[i]=(int) (getY()+(yAlasPoints[i]*getProportion())*-1);
+			
+		}
+		
+		int xCabinaPoints[] = { 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 2 };
+		int yCabinaPoints[] = { 0, 2, 2, 4, 4, 6, 6, 4, 4, 2, 2, 0, 0 };
+		int nCabinaPoints = 13;
+		g.setColor(Color.RED);
+		g.fillPolygon(xAlasProPoints, yAlasProPoints, nAlasPoints);
+		g.setColor(Color.CYAN);
+		g.fillPolygon(xCabinaPoints, yCabinaPoints, nCabinaPoints);
+
+	}
+	public int getProportion() {
+		return proportion;
+	}
+
+	public void setProportion(int proportion) {
+		this.proportion = proportion;
+	}
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getDirX() {
+		return dirX;
+	}
+
+	public void setDirX(double dirX) {
+		this.dirX = dirX;
+	}
+
+	public boolean isLeftAccel() {
+		return leftAccel;
+	}
+
+	public void setLeftAccel(boolean leftAccel) {
+		this.leftAccel = leftAccel;
+	}
+
+	public boolean isRightAccel() {
+		return rightAccel;
+	}
+
+	public void setRightAccel(boolean rightAccel) {
+		this.rightAccel = rightAccel;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 
 }
--- src/com/gmail/pablo23dr/killTheNazi/MainClass.java
+++ src/com/gmail/pablo23dr/killTheNazi/MainClass.java
@@ -1,5 +1,104 @@
 package com.gmail.pablo23dr.killTheNazi;
 
-public class MainClass {
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MainClass extends Applet implements Runnable, KeyListener{
+
+	Graphics gfx;
+	Image img;
+	Thread thread;
+	boolean GameOver;
+	HumanShip humanShip;
+	EnemyShip enemyShip;
+	Bullet bullet;
+	
+	public void init() {
+		this.resize(600, 600);
+		img = createImage(600, 600);
+		gfx = img.getGraphics();
+		this.addKeyListener(this);
+		GameOver = false;
+		humanShip = new HumanShip();
+		
+		
+		thread = new Thread(this);
+		thread.start();
+		
+	}	
+	
+	public void paint(Graphics g) {
+		gfx.setColor(Color.BLACK);
+		gfx.fillRect(0, 0, 600, 600);
+		humanShip.draw(gfx);
+		
+		
+		g.drawImage(img, 0, 0, null);
+	}
+	
+	public void update(Graphics g) {
+		paint(g);
+	}
+	
+	public void run() {
+		for(;;) {
+			
+			
+			
+			
+			
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	/**
+	 * comprobador de reglas del juego
+	 */
+	public void checkGameOver() {
+		
+	}
+	/**
+	 * controles del juego
+	 */
+	public void keyPressed(KeyEvent e) {
+		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			
+		}
+		
+		
+	}
+
+	
+	public void keyReleased(KeyEvent e) {
+		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			
+		}
+		
+	}
+
+	
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	
+
 
 }
